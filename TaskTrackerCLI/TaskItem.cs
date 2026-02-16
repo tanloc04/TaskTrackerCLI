660cf2045f4a26b6e12d737d149ea96aa05e695d
Nguyen Tan Loc <126189862+tanloc04@users.noreply.github.com>
2026-02-16 11:29:38 +0700

    add TaskItem class

---
+++ TaskTrackerCLI/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace TaskTrackerCLI
+{
+    public class TaskItem
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; } = "todo";
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
